fec8ca8cd10a8fcb20deac9e74328fb362c9acde
antonylemos <lemosantony@gmail.com>
2024-05-05 21:38:16 -0300

    feat: add get balance route

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,6 +3,7 @@ const AccountsController = require('./app/controllers/AccountsController');
 
 const router = Router();
 
+router.get('/accounts/:id', AccountsController.show);
 router.post('/accounts', AccountsController.store);
 
 module.exports = router;
