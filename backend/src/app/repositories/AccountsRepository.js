95d565de717dbeb11d47ca5b1d879f384a4fa6b2
antonylemos <lemosantony@gmail.com>
2024-05-05 21:37:41 -0300

    feat: create find by id method

--- backend/src/app/repositories/AccountsRepository.js
+++ backend/src/app/repositories/AccountsRepository.js
@@ -1,6 +1,12 @@
 const db = require('../../database');
 
 class AccountsRepository {
+  async findById(id) {
+    const [row] = await db.query('SELECT * FROM accounts WHERE id = $1', [id]);
+
+    return row;
+  }
+
   async create({ id }) {
     const [row] = await db.query(`
       INSERT INTO accounts(id)
