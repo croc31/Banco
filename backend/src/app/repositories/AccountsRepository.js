627e27f16207f2ffc9a2796f044d100d457acea1
Clauber Câmara <clauberogerioo@gmail.com>
2024-05-06 20:49:08 -0300

    fix: correcao na nomenclatura do valor

--- backend/src/app/repositories/AccountsRepository.js
+++ backend/src/app/repositories/AccountsRepository.js
@@ -17,8 +17,8 @@ class AccountsRepository {
     return row;
   }
   async credit(id, value) {
-    const [oldValue] = await db.query('select value FROM accounts WHERE id = $1', [id]);
-    const [row] = await db.query('UPDATE accounts SET value = $2 WHERE id = $1', [id][value-oldValue[0]]);
+    const [oldValue] = await db.query('select balance FROM accounts WHERE id = $1', [id]);
+    const [row] = await db.query('UPDATE accounts SET balance = $2 WHERE id = $1', [id][value-oldValue[0]]);
 
     console.log({ row });
 
