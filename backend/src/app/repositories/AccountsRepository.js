5ddf15f3fc09d3073cbf0dc6ebfdf7aaf712dd10
Clauber Câmara <clauberogerioo@gmail.com>
2024-05-06 19:45:13 -0300

    feat:débito feito

--- backend/src/app/repositories/AccountsRepository.js
+++ backend/src/app/repositories/AccountsRepository.js
@@ -16,6 +16,15 @@ class AccountsRepository {
 
     return row;
   }
+
+  async debit(id, value) {
+    const [oldValue] = await db.query('select value FROM accounts WHERE id = $1', [id]);
+    const [row] = await db.query('UPDATE accounts SET value = $2 WHERE id = $1', [id][value+oldValue[0]]);
+
+    console.log({ row });
+
+    return row;
+  }
 }
 
 module.exports = new AccountsRepository();
